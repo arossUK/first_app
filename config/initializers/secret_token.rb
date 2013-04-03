5ce4541d11a5ab7c0ab4c5739eb4b0b1b9bc68cf
arossUK <aross@mills.edu>
2013-04-02 19:51:12 -0700

    Adding initial commit with change to the vim

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '1149400517fdf3a64467210186742b2f2965874105590c083b5c1ef3029c37cbf3f873cb5777704c046e4c6d670cb24db780bcd4a12355b8a2abfec5891b0166'
